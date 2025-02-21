dfa8089400039db564dbda00dbdb30c580436808
SabirinMohamedAli <thaprinmohamett1333@gmail.com>
2025-02-21 23:21:43 +0300

    updated dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -744,7 +744,7 @@ const Dashboard = () => {
       <div className="flex-1 lg:ml-64 p-8 bg-gray-100 min-h-screen">
         <div className="flex justify-between items-center mb-8">
           <h1 className="text-4xl font-semibold">
-            Welcome to the Admin Dashboard
+            Welcome To the Admin Dashboard
           </h1>
         </div>
 
